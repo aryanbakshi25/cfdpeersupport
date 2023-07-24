1c62d8e0806e74f3b39511bf97eaed60b5522df0
aryanbakshi25 <aryanbakshi2509@gmail.com>
2023-07-24 16:07:40 -0400

    got rid of 'public' folder

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ const express = require('express');
 const app = express();
 
 // Set up the public directory
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // Serve the index.html file
-app.get('/', async(req, res) => {
-    res.send(await readFile('./public/index.html', 'utf8'));
+// app.get('/', async(req, res) => {
+//     res.send(await readFile('./public/index.html', 'utf8'));
+// });
+ app.get('/', async(req, res) => {
+     res.send(await readFile('index.html', 'utf8'));
 });
 
 // Server the CSS files
-app.use(express.static('public/css files'));
+// app.use(express.static('public/css files'));
+app.use(express.static('css files'));
 
 // Serve the image files
-app.use(express.static('public/images'));
+// app.use(express.static('public/images'));
+app.use(express.static('images'));
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`));
